a18cc4d077c213a742540f83af7d9b61f2697269
CJ Patoilo <cjpatoilo@gmail.com>
2018-06-28 23:21:17 -0300

    Add sentry

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { hot } from 'react-hot-loader'
 import Routes from 'react-static-routes'
 import ReactDOM from 'react-dom'
 import GoogleFonts from 'google-fonts'
+import Raven from 'raven-js'
 import ReactGA from 'react-ga'
 import 'font-awesome/css/font-awesome.css'
 import 'normalize.css'
@@ -22,6 +23,8 @@ export default hot(module)(App)
 if (typeof document !== 'undefined') {
   ReactGA.initialize('UA-24389952-13')
   ReactGA.pageview(window.location.pathname)
+
+  Raven.config('https://f99ae607fb73442c8f006342191d6204@sentry.io/1234637').install()
 
   if ('serviceWorker' in navigator && window.location.protocol === 'https:') navigator.serviceWorker.register('/service-worker.js')
   else console.info(`[Offline] Don't support service work!`)
