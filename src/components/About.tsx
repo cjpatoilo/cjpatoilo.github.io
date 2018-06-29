baa72b96202ecfe7468975226f9cb1e616b88705
CJ Patoilo <cjpatoilo@gmail.com>
2018-06-28 23:27:39 -0300

    Add about component

---
+++ src/components/About.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import './About.css'
+
+export default ({}): JSX.Element => (
+  <div className="About">
+    Trying to make a beautiful web, fast, 100% responsive, and accessible for everyone.
+  </div>
+)
