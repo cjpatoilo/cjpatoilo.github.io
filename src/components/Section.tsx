a9b18d28457f5ebf2e3fdcf245e8d15e52cbf52e
CJ Patoilo <cjpatoilo@gmail.com>
2018-07-03 22:52:45 -0300

    Lint the code

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 import './Section.css'
 
-export default ({ children, modifier }: { children: any, modifier?: string }): JSX.Element => (
+export default ({
+  children,
+  modifier
+}: {
+children: any
+modifier?: string
+}): JSX.Element => (
   <section className={modifier ? `Section--${modifier}` : 'Section'}>
     {children}
   </section>
