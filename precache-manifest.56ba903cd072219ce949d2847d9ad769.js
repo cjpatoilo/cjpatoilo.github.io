43202a73845085f84b63109291848800dba04d88
CJ Patoilo <cjpatoilo@gmail.com>
2021-01-04 16:45:42 -0300

    Updates

---
+++ precache-manifest.56ba903cd072219ce949d2847d9ad769.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41355dc1f40768fb3b8e5435212ed1c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4dfc18c969064598e48e",
+    "url": "/static/css/2.e767db07.chunk.css"
+  },
+  {
+    "revision": "4dfc18c969064598e48e",
+    "url": "/static/js/2.ed6b9bd8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ed6b9bd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "738fc220fc2402f86026",
+    "url": "/static/js/main.2eb88068.chunk.js"
+  },
+  {
+    "revision": "06da02ec83927aa7b381",
+    "url": "/static/js/runtime-main.b423a13d.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
